ef4a6fe80e73286af3fd8d65698f4e43d252123d
Yoni <yonimizan2006@gmail.com>
2023-05-14 17:35:26 +0300

    added

---
+++ client/src/routes/routesModel.js
@@ -0,0 +1,12 @@
+
+const ROUTES = {
+    ROOT: "/cards",
+    ABOUT: "/about",
+    CARDS: "/cards",
+    SANDBOX:"/sandbox",
+    SIGNUP:"/signUp",
+    FAV_CARDS: "/Favorites"
+    
+};
+
+export default ROUTES;
