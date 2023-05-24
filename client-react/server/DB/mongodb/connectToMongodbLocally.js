9269e22e4584a1f9312428073666e748e0f0af72
Yoni <yonimizan2006@gmail.com>
2023-05-24 20:47:10 +0300

    added

--- client-react/server/DB/mongodb/connectToMongodbLocally.js
+++ client-react/server/DB/mongodb/connectToMongodbLocally.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const chalk = require("chalk");
 
 mongoose
-  .connect("mongodb://localhost:27017/business_card_app")
+  .connect("mongodb://127.0.0.1:27017/business_card_app")
   .then(() => console.log(chalk.magentaBright("connected to MongoDb Locally!")))
   .catch(error =>
     console.log(chalk.redBright.bold(`could not connect to mongoDb: ${error}`))
